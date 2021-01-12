182f9dbbd81fabfeb962b5aa1ba54a45e62abee7
Yousaf Raza <yousafraza39142@gmail.com>
2021-01-12 07:24:53 +0500

    done

--- src/js/index.js
+++ src/js/index.js
@@ -3,52 +3,52 @@ AOS.init();
 particlesJS.load('particles-js', '../particles.json');
 
 
-// const bodyAnimation = anime({
-//     targets: 'body',
-//     easing: "linear",
-//     duration: 1000,
-//     opacity: [0, 1],
-//     autoplay: false
-// })
-//
-// const animation = anime.timeline({
-//     easing: 'easeOutQuart',
-//     duration: 2000,
-//     autoplay: false,
-// });
-//
-// animation.add({
-//     targets: "introduction-section > pic > .profile-pic",
-//     delay: 500,
-//     translateY: ['-10%', '0'],
-//     opacity: [0, 1]
-// }).add({
-//     targets: "introduction-section > name > div#name",
-//     translateX: ['-5%', '0'],
-// }, '-=1000').add({
-//     targets: "introduction-section > name > div#name",
-//     opacity: [0, 1]
-// }, '-=2000').add({
-//     targets: "introduction-section > name > div#position",
-//     opacity: [0, 1],
-//     translateX: ['5%', 0]
-// }, "-=1500").add({
-//     targets: "introduction-section > name > div.cir-btn > i",
-//     opacity: [0,1],
-//     duration: 1000,
-//     delay: anime.stagger(100),
-//     easing: 'spring(1, 60, 10, 0)'
-// }, "-=1500")
-//
+const bodyAnimation = anime({
+    targets: 'body',
+    easing: "linear",
+    duration: 1000,
+    opacity: [0, 1],
+    autoplay: false
+})
+
+const animation = anime.timeline({
+    easing: 'easeOutQuart',
+    duration: 2000,
+    autoplay: false,
+});
+
+animation.add({
+    targets: "introduction-section > pic > .profile-pic",
+    delay: 500,
+    translateY: ['-10%', '0'],
+    opacity: [0, 1]
+}).add({
+    targets: "introduction-section > name > div#name",
+    translateX: ['-5%', '0'],
+}, '-=1000').add({
+    targets: "introduction-section > name > div#name",
+    opacity: [0, 1]
+}, '-=2000').add({
+    targets: "introduction-section > name > div#position",
+    opacity: [0, 1],
+    translateX: ['5%', 0]
+}, "-=1500").add({
+    targets: "introduction-section > name > div.cir-btn > i",
+    opacity: [0,1],
+    duration: 1000,
+    delay: anime.stagger(100),
+    easing: 'spring(1, 60, 10, 0)'
+}, "-=1500")
+
 
 
 window.onload = () => {
-    // bodyAnimation.play();
-    // setTimeout(() => {
-    //     animation.play();
-    // },1000);
+    bodyAnimation.play();
+    setTimeout(() => {
+        animation.play();
+    },1000);
 
-    // window.scrollTo({top:0, behavior:"smooth"});
+    window.scrollTo({top:0, behavior:"smooth"});
 
 };
 
